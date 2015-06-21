596cb81b6f6daed831c6c03dbc300a6964771e2b
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-06-21 15:58:28 +0000

    Updated the framework for better handling of source parameter. Bump to version 0.9.1. Removed the pollInterval because we cannot specifiy a pollInterval per item by now. The pollInterval always defaults to 1000ms.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- init.lua
+++ init.lua
@@ -11,12 +11,10 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
---
 
 local framework = require('framework.lua')
 local Plugin = framework.Plugin
 local MeterDataSource = framework.MeterDataSource
-local notEmpty = framework.string.notEmpty
 local isEmpty = framework.string.isEmpty
 local pack = framework.util.pack
 local urldecode = framework.string.urldecode
@@ -32,20 +30,18 @@ local plugin = Plugin:new(params, ds)
 
 function plugin:onParseValues(data)
   local result = {}
-  local source = self.source
   for i, v in pairs(data) do
     if v.metric == 'system.fs.use_percent.total' then
       result['DISKUSE_SUMMARY'] = { value = v.value, timestamp = v.timestamp }
     else
-      local metric, rest = string.match(v.metric, '(system%.fs%.use_percent)|(.*)')
+      local _, rest = string.match(v.metric, '(system%.fs%.use_percent)|(.*)')
       if rest then
         local dir, dev = string.match(rest, '^dir=(.+)&dev=(.+)')
         dir = urldecode(dir)
         dev = urldecode(dev)
         for _, item in ipairs(params.items) do
           if (item.dir == dir and item.device == dev) or (item.dir and isEmpty(item.device) and item.dir == dir) or (isEmpty(item.dir) and item.device and item.device == dev) then
-            source = self.source .. '.' .. (item.diskname or dir .. '.' .. dev)
-            source = string.gsub(source, "([!@#$%%^&*() {}<>/\\|]", "-")
+            local source = self.source .. '.' .. (item.diskname or dir .. '.' .. dev)
             table.insert(result, pack('DISKUSE_SUMMARY', v.value, v.timestamp, source))
             break
           end
